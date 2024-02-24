bfb3fc82025f45fd946105f32365b921d98048f7
hp533 <hp533@cs.drexel.edu>
2024-02-24 03:29:09 -0500

    created the MasterControl class

---
+++ src/main/java/MasterControl.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class MasterControl {
+	private CommandValidator commandValidator;
+	private CommandProcessor commandProcessor;
+	private CommandStorage commandStorage;
+
+	public MasterControl(CommandValidator commandValidator, CommandProcessor commandProcessor,
+			CommandStorage commandStorage) {
+		this.commandValidator = commandValidator;
+		this.commandProcessor = commandProcessor;
+		this.commandStorage = commandStorage;
+	}
+
+	public List<String> start(List<String> input) {
+		for (String command : input) {
+			String[] parts = command.stripTrailing().split(" ");
+			if (commandValidator.validate(parts)) {
+				commandProcessor.handle(parts);
+			} else {
+				commandStorage.store(command);
+			}
+
+		}
+		return commandStorage.getInvalidCommands();
+	}
+}
