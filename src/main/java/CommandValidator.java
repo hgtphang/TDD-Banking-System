510cce314b640f94c1063be414f64286864ee87b
hp533 <hp533@cs.drexel.edu>
2024-02-28 02:40:18 -0500

    modified CommandValidator class with two commands: create and deposit

--- src/main/java/CommandValidator.java
+++ src/main/java/CommandValidator.java
@@ -2,53 +2,22 @@ class CommandValidator {
 
 	private final Bank bank;
 
-	CommandValidator(Bank bank) {
+	public CommandValidator(Bank bank) {
 		this.bank = bank;
 	}
 
-	boolean validate(String command) {
-		String[] parts = command.split("\\s+");
-		if (parts[0].equals("create")) {
-			return createValidator(parts);
-		} else if (parts[0].equals("deposit")) {
-			return depositValidator(parts);
+	public boolean validate(String str) {
+		String[] parts = str.stripTrailing().split(" ");
+
+		if (parts[0].equalsIgnoreCase("create")) {
+			CreateCommandValidator createCommandValidator = new CreateCommandValidator(bank);
+			return createCommandValidator.validate(str);
+		} else if (parts[0].equalsIgnoreCase("deposit")) {
+			DepositCommandValidator depositCommandValidator = new DepositCommandValidator(bank);
+			return depositCommandValidator.validate(str);
 		} else {
 			return false;
 		}
-	}
 
-	public boolean createValidator(String[] parts) {
-		if (parts.length < 4) {
-			return false;
-		}
-
-		if (parts[1].equals("cd") && parts.length < 5) {
-			return false;
-		}
-
-		try {
-			if (parts[1].equals("cd")) {
-				double balance = Double.parseDouble(parts[4]);
-				if (balance < 0) {
-					return false;
-				}
-			}
-		} catch (NumberFormatException e) {
-			return false;
-		}
-
-		int accountId = Integer.parseInt(parts[2]);
-		if (bank.getAccountById(accountId) != null) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public boolean depositValidator(String[] parts) {
-		if (parts.length != 3) {
-			return false;
-		}
-		return true;
 	}
 }
