bbe917278f211dd657d03eb5e9e5eb9a9579bdcb
hp533 <hp533@cs.drexel.edu>
2024-03-14 01:59:54 -0400

    add more test cases for withdraw command validator to kill survived mutants

--- src/test/java/banking/WithdrawCommandValidatorTest.java
+++ src/test/java/banking/WithdrawCommandValidatorTest.java
@@ -50,6 +50,34 @@ public class WithdrawCommandValidatorTest {
 	}
 
 	@Test
+	void withdraw_a_right_amount_from_checking_account_is_valid() {
+		bank.createCheckingAccount(12345678, 2.3);
+		boolean actual = commandValidator.validate("withdraw 12345678 300");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_negative_amount_from_checking_is_invalid() {
+		bank.createCheckingAccount(12345678, 2.3);
+		boolean actual = commandValidator.validate("withdraw 12345678 -200");
+		assertFalse(actual);
+	}
+
+	@Test
+	void withdraw_a_minimum_amount_from_checking_account_is_valid() {
+		bank.createCheckingAccount(12345678, 2.1);
+		boolean actual = commandValidator.validate("withdraw 12345678 0");
+		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_a_maximum_amount_from_checking_account_is_valid() {
+		bank.createCheckingAccount(12345678, 2.1);
+		boolean actual = commandValidator.validate("withdraw 12345678 400");
+		assertTrue(actual);
+	}
+
+	@Test
 	void withdraw_more_than_allowed_from_savings_account_is_invalid() {
 		bank.createSavingsAccount(23456789, 1.2);
 		boolean actual = commandValidator.validate("withdraw 23456789 2000");
@@ -57,23 +85,16 @@ public class WithdrawCommandValidatorTest {
 	}
 
 	@Test
-	void withdraw_zero_amount_is_valid() {
+	void withdraw_minimum_amount_from_savings_is_valid() {
 		bank.createSavingsAccount(12345678, 1.3);
 		boolean actual = commandValidator.validate("withdraw 12345678 0");
 		assertTrue(actual);
 	}
 
 	@Test
-	void withdraw_negative_amount_is_invalid() {
-		bank.createCheckingAccount(12345678, 2.3);
-		boolean actual = commandValidator.validate("withdraw 12345678 -200");
-		assertFalse(actual);
-	}
-
-	@Test
-	void withdraw_a_right_amount_from_checking_account_is_valid() {
-		bank.createCheckingAccount(12345678, 2.3);
-		boolean actual = commandValidator.validate("withdraw 12345678 300");
+	void withdraw_maximum_amount_from_savings_is_valid() {
+		bank.createSavingsAccount(12345678, 1.3);
+		boolean actual = commandValidator.validate("withdraw 12345678 1000");
 		assertTrue(actual);
 	}
 
@@ -82,6 +103,13 @@ public class WithdrawCommandValidatorTest {
 		bank.createSavingsAccount(23456789, 3.2);
 		boolean actual = commandValidator.validate("withdraw 23456789 900");
 		assertTrue(actual);
+	}
+
+	@Test
+	void withdraw_a_negative_amount_from_savings_account_is_valid() {
+		bank.createSavingsAccount(23456789, 3.2);
+		boolean actual = commandValidator.validate("withdraw 23456789 -100");
+		assertFalse(actual);
 	}
 
 	@Test
