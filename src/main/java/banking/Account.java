92347ced57737fa7d65f31a99953c0c2722ef0b8
hp533 <hp533@cs.drexel.edu>
2024-03-14 09:45:46 -0400

    modify deposit and withdraw method

--- src/main/java/banking/Account.java
+++ src/main/java/banking/Account.java
@@ -1,29 +1,19 @@
 package banking;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class Account {
 	private int id;
 	private double apr;
 	private double balance;
-	private List<String> accountType;
 
 	public Account(int id, double apr) {
 		this.id = id;
 		this.apr = apr;
-		accountType = new ArrayList<>();
 	}
 
 	public Account(int id, double apr, double balance) {
 		this.id = id;
 		this.apr = apr;
 		this.balance = balance;
-		accountType = new ArrayList<>();
-	}
-
-	public List<String> getAccountType() {
-		return accountType;
 	}
 
 	public int getId() {
@@ -38,17 +28,15 @@ public abstract class Account {
 		return balance;
 	}
 
-	public double deposit(double depositAmount) {
+	public void deposit(double depositAmount) {
 		balance += depositAmount;
-		return balance;
 	}
 
-	public double withdraw(double withdrawAmount) {
+	public void withdraw(double withdrawAmount) {
 		if (withdrawAmount > balance) {
 			balance = 0;
 		} else {
 			balance -= withdrawAmount;
 		}
-		return balance;
 	}
 }
