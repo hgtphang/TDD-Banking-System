2afa229e449cbf59248b05a8d6703f2855b5e56c
hp533 <hp533@cs.drexel.edu>
2024-03-17 00:53:53 -0400

    add transfer processor to command processor class

--- src/main/java/banking/CommandProcessor.java
+++ src/main/java/banking/CommandProcessor.java
@@ -10,13 +10,17 @@ public class CommandProcessor {
 
 	public void handle(String str) {
 		String[] parts = str.stripTrailing().split(" ");
-		if (parts[0].equals("create")) {
+
+		if (parts[0].equalsIgnoreCase("create")) {
 			CreateCommandProcessor createCommandProcessor = new CreateCommandProcessor(bank);
 			createCommandProcessor.createHandler(parts);
-		} else if (parts[0].equals("deposit")) {
+		} else if (parts[0].equalsIgnoreCase("deposit")) {
 			DepositCommandProcessor depositCommandProcessor = new DepositCommandProcessor(bank);
 			depositCommandProcessor.depositHandler(parts);
-		} else if (parts[0].equals("withdraw")) {
+		} else if (parts[0].equalsIgnoreCase("withdraw")) {
+			WithdrawCommandProcessor withdrawCommandProcessor = new WithdrawCommandProcessor(bank);
+			withdrawCommandProcessor.withdrawHandler(parts);
+		} else if (parts[0].equalsIgnoreCase("transfer")) {
 			WithdrawCommandProcessor withdrawCommandProcessor = new WithdrawCommandProcessor(bank);
 			withdrawCommandProcessor.withdrawHandler(parts);
 		}
