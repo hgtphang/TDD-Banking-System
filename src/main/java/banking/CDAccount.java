7d22812b44e49d08395b99e56b7e64313085c763
hp533 <hp533@cs.drexel.edu>
2024-03-17 14:32:50 -0400

    implement pass method for cd and savings account

--- src/main/java/banking/CDAccount.java
+++ src/main/java/banking/CDAccount.java
@@ -4,4 +4,12 @@ public class CDAccount extends Account {
 	public CDAccount(int id, double apr, double balance) {
 		super(id, apr, balance);
 	}
+
+	@Override
+	public void pass(int months) {
+		setAge(months);
+		if (getAge() >= 12) {
+			alreadyWithdrawn = false;
+		}
+	}
 }
--- src/main/java/banking/SavingsAccount.java
+++ src/main/java/banking/SavingsAccount.java
@@ -4,4 +4,10 @@ public class SavingsAccount extends Account {
 	public SavingsAccount(int id, double apr) {
 		super(id, apr);
 	}
+
+	@Override
+	public void pass(int months) {
+		setAge(months);
+		alreadyWithdrawn = false;
+	}
 }
