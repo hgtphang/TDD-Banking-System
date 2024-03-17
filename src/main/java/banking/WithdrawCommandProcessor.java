ff1c6bee16477923ea32872f89b88c70cbee8730
hp533 <hp533@cs.drexel.edu>
2024-03-17 00:42:10 -0400

    update withdrawHandler for withdraw processor

--- src/main/java/banking/WithdrawCommandProcessor.java
+++ src/main/java/banking/WithdrawCommandProcessor.java
@@ -9,5 +9,8 @@ public class WithdrawCommandProcessor {
 
 	public void withdrawHandler(String[] parts) {
 		int accountID = Integer.parseInt(parts[1]);
+		double amount = Double.parseDouble(parts[2]);
+
+		bank.getAccountById(accountID).withdraw(amount);
 	}
 }
