feffbf712e3f2202d2c5811c558df908922ee8bf
hp533 <hp533@cs.drexel.edu>
2024-03-17 00:57:15 -0400

    create transfer processor class

---
+++ src/main/java/banking/TransferCommandProcessor.java
@@ -0,0 +1,12 @@
+package banking;
+
+public class TransferCommandProcessor {
+	private final Bank bank;
+
+	public TransferCommandProcessor(Bank bank) {
+		this.bank = bank;
+	}
+
+	public void transferHandler(String[] parts) {
+	}
+}
